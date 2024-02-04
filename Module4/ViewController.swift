1d1e78eb42be58e6a73f813e15e9b20168216110
Nathen Wiley <nzw0037@auburn.edu>
2024-02-03 23:46:36 -0800

    Simple clock and timer

--- Module4/ViewController.swift
+++ Module4/ViewController.swift
@@ -5,15 +5,103 @@
 //  Created by user250623 on 2/3/24.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    var countdownTimer: Timer?
+    var remainingSeconds: Int = 0
+    var player: AVAudioPlayer?
+    var backgroundTimer: Timer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupLiveClock()
+        setupBackgroundTimer()
+    }
+    
+    func setupLiveClock() {
+        updateClock()
+    }
+
+    func setupBackgroundTimer() {
+        backgroundTimer = Timer.scheduledTimer(withTimeInterval: 60, repeats: true) { [weak self] _ in
+            self?.updateBackgroundImage()
+        }
+    }
+
+    func updateClock() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "E, dd MMM yyyy HH:mm:ss"
+        label1.text = dateFormatter.string(from: Date())
+        updateBackgroundImage()
+    }
+
+    func updateBackgroundImage() {
+        let hour = Calendar.current.component(.hour, from: Date())
+        let imageName = hour < 12 ? "dayImage" : "nightImage"
+        backgroundImageView.image = UIImage(named: imageName)
     }
 
 
-}
+    @IBAction func actionButtonTapped(_ sender: UIButton) {
+        if countdownTimer == nil {
+            startCountdown()
+        } else {
+            stopMusicAndReset()
+        }
+    }
 
+    func startCountdown() {
+        remainingSeconds = Int(datePicker.countDownDuration)
+        updateCountdownLabel()
+        
+        countdownTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
+            self?.updateCountdown()
+        }
+        
+        actionButton.setTitle("Stop Music", for: .normal)
+    }
+
+    func updateCountdown() {
+        if remainingSeconds > 0 {
+            remainingSeconds -= 1
+            updateCountdownLabel()
+        } else {
+            countdownTimer?.invalidate()
+            countdownTimer = nil
+            playMusic()
+        }
+    }
+
+    func updateCountdownLabel() {
+        let hours = remainingSeconds / 3600
+        let minutes = (remainingSeconds % 3600) / 60
+        let seconds = (remainingSeconds % 3600) % 60
+        label2.text = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+    }
+
+    func playMusic() {
+        guard let url = Bundle.main.url(forResource: "Music", withExtension: "mp3") else { return }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        actionButton.setTitle("Stop Music", for: .normal)
+    }
+
+    func stopMusicAndReset() {
+        player?.stop()
+        actionButton.setTitle("Start Timer", for: .normal)
+        datePicker.isEnabled = true // Re-enable the date picker to allow setting a new timer
+    }
+}
